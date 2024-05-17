b566b09b7560b1613f30ec98bd4a67c40f15d605
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-05-17 22:45:23 +0900

    helper function for dirname

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const expressRouter = express.Router();
 
 expressRouter.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 expressRouter.post("/product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const expressRouter = express.Router();
 
 // get (not 'use')
 expressRouter.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = expressRouter;
